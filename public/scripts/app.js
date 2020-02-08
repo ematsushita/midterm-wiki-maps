9579efe4a0377ab6a336e05b2155a0c1ed9c740b
Jake Robins <mendahu@gmail.com>
2020-02-08 17:16:47 +0000

    got a map on the page, woot

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,5 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+function initMap() {
+  const lhl = {lat: 49.281228, lng: -123.114669};
+  const map = new google.maps.Map(document.getElementById('map'), {zoom: 8, center: lhl});
+  const marker = new google.maps.Marker({position: lhl, map: map});
+}
