536257a8a4b6869f6a55a2bc66bd33592dd9606f
Jake Robins <mendahu@gmail.com>
2020-02-08 19:39:28 +0000

    map now accepts bounds and dynamic zoom

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,8 +2,4 @@ $(document).ready(function() {
 
 });
 
-function initMap() {
-  const lhl = {lat: 49.281228, lng: -123.114669};
-  const map = new google.maps.Map(document.getElementById('map'), {zoom: 8, center: lhl});
-  const marker = new google.maps.Marker({position: lhl, map: map});
-}
+
