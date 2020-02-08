cdf739a8f8d9313dc1d0c3caf11f785f61f6d1cf
Jake Robins <mendahu@gmail.com>
2020-02-08 02:15:35 +0000

    fixed server typo

--- routes/listsRouting.js
+++ routes/listsRouting.js
@@ -9,7 +9,7 @@ module.exports = (db) => {
       (owner_id, title, description)
       VALUES
       ($1, $2, $3)
-      RETURNING *`, [ownerId, title, desc])
+      RETURNING *`, [ownerId, title, desc]);
     };
     const owner_id = req.session.user.id;
     const title = req.body.title;
@@ -17,20 +17,20 @@ module.exports = (db) => {
 
     addList(owner_id, title, desc)
 
-      .then (res => {
+      .then(res => {
         if (res.rows.length) {
-        let list_id = res.rows[0]["id"]
-        return response.redirect(`/${list_id}`);
-      }
-    });
+          let list_id = res.rows[0]["id"];
+          return response.redirect(`/lists/${list_id}`);
+        }
+      });
 
-  router.get("/:id", (req, res) => {
-    let templateVars = {
-      user: req.session.user,
-      list: req.params.id,
-    }
-    res.render("../map", templateVars)
-  })
+    router.get("/:id", (req, res) => {
+      let templateVars = {
+        user: req.session.user,
+        list: req.params.id,
+      };
+      res.render("map", templateVars);
+    });
   });
 
   return router;
