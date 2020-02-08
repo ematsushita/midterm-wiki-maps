1e2e7412eba06906b7f17bf2ee6e5b30ab4ca5a7
Elissa <elissa.matsushita@gmail.com>
2020-02-08 18:32:46 +0000

    implement Get Points function to render list by  id

--- routes/listsRouting.js
+++ routes/listsRouting.js
@@ -22,15 +22,28 @@ module.exports = (db) => {
           let list_id = res.rows[0]["id"];
           return response.redirect(`/lists/${list_id}`);
         }
-      });
+      })
+  });
 
-    router.get("/:id", (req, res) => {
+  router.get("/:id", (req, response) => {
+    const getPoints = function(list_id) {
+      return db.query(`
+      SELECT * FROM points
+      WHERE id = $1
+      `, [list_id]
+      );
+    };
+
+    getPoints(req.params.id)
+
+    .then(res => {
       let templateVars = {
         user: req.session.user,
         list: req.params.id,
+        points: res.rows
       };
-      res.render("map", templateVars);
-    });
+      return response.render("map", templateVars);
+    })
   });
 
   return router;
