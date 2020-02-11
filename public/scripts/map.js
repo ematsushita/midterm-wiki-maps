ddb18db3af2dab62428fa69043a94a2398ee90a5
Jake Robins <mendahu@gmail.com>
2020-02-11 01:34:43 +0000

    info windows now display

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,17 +1,54 @@
+let infoWindow = null;
+
+const buildInfoWindow = function(title, desc, imgUrl) {
+  infoWindow = null;
+
+  const contentString = '<div id="content">' +
+  '<div id="siteNotice">' +
+  '</div>' +
+  `<img src="${imgUrl}" style="width: 40%; float: right;">` +
+  `<h1 id="firstHeading" class="firstHeading">${title}</h1>` +
+  '<div id="bodyContent">' +
+  `<p>${desc}</p>` +
+  '</div>' +
+  '</div>';
+
+  infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+  return infoWindow;
+};
+
+// pin.addListener("click", function() {
+//   info.open(map, pin);
+// });
+
+const placeMarker = function(marker, point, map) {
+
+  //newMarker is the marker object
+  const newMarker = new google.maps.Marker({position: marker, map: map});
+
+  //adds an event listener on click to craete an info window and display it
+  google.maps.event.addListener(newMarker, 'click', function() {
+    if (infoWindow) infoWindow.close();
+    buildInfoWindow(point.title, point.description, point.img_url);
+    infoWindow.open(map, newMarker);
+  });
+
+  return newMarker;
+};
+
 const initMap = function(mapBounds, markerPoints) {
   const bounds = new google.maps.LatLngBounds(mapBounds[0], mapBounds[1]);
-  const centre = bounds.getCenter();
+  const mapCentre = bounds.getCenter();
 
-  const map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: centre});
+  const map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: mapCentre});
 
-  const placeMarker = function(marker) {
-    new google.maps.Marker({position: marker, map: map});
-  };
-
-  markerPoints.forEach((element, index) => {
-    const marker = {lat: element.latitude, lng: element.longitude};
-    placeMarker(marker, index);
-    bounds.extend(marker);
+  markerPoints.forEach(point => {
+    const location = {lat: point.latitude, lng: point.longitude};
+    placeMarker(location, point, map);
+    bounds.extend(location);
   });
 
   map.fitBounds(bounds, 5);
@@ -38,7 +75,6 @@ $(document).ready(function() {
       return getPoints();
     })
     .then(value => {
-      console.log(coords, value);
       initMap(coords, value);
     });
 
