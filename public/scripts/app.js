28419a9a4b7858e55e8c19dafd65517859936ce1
Jake Robins <mendahu@gmail.com>
2020-02-12 20:21:59 +0000

    complete test of retabling front page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,6 +24,80 @@ $(document).ready(function() {
     }
   };
 
+  const createAccordionCard = function(card, index, path, container) {
+
+    console.log(container[0].id);
+
+    const $card = $('<div>')
+      .addClass("card");
+
+    const $cardHeader = $("<div>")
+      .addClass("card-header")
+      .addClass("py-1")
+      .addClass("d-flex")
+      .addClass("justify-content-between")
+      .addClass("align-items-center")
+      .attr('id', `heading-${path}-${card.id}`)
+      .appendTo($card);
+
+    const $leftHeadingWrap = $("<div>")
+      .appendTo($cardHeader);
+
+    const $cardHeading = $("<h2>")
+      .addClass("mb-0")
+      .appendTo($leftHeadingWrap);
+
+    const $favBadge = $('<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>')
+      .appendTo($cardHeading)
+      .on("click", () => {
+        toggleFav(card.id, card.owner_id);
+        togglePos(card);
+      });
+
+    if (card.fave_id) $favBadge.addClass("favourited-heart");
+
+    const $cardButton = $("<button>")
+      .addClass("btn")
+      .addClass("btn-link")
+      .attr('type', 'button')
+      .attr('data-toggle', 'collapse')
+      .attr('data-target', `#collapse-${path}-${card.id}`)
+      .attr('aria-expanded', 'true')
+      .attr('area-controls', `collapse-${path}-${card.id}`)
+      .text(card.title)
+      .appendTo($cardHeading);
+
+    const $rightHeadingWrapper = $("<div>")
+      .appendTo($cardHeader);
+
+    const $viewButton = $("<a>")
+      .html("View")
+      .attr("href", `/lists/${card.id}`)
+      .addClass("btn btn-purple text-white my-2")
+      .appendTo($rightHeadingWrapper);
+
+    const $cardBodyWrapper = $("<div>")
+      .attr('id', `collapse-${path}-${card.id}`)
+      .addClass("collapse")
+      //.addClass("show")
+      .attr("aria-labelledby", `heading-${path}-${card.id}`)
+      .attr("data-parent", `#${container[0].id}`)
+      .appendTo($card);
+
+    const $cardBody = $("<div>")
+      .addClass("card-body")
+      .text(card.description)
+      .appendTo($cardBodyWrapper);
+
+    if (index === 0) {
+      //$cardBodyWrapper.addClass("show");
+    } else {
+      $cardButton.addClass("collapsed");
+    }
+
+    return $card;
+  };
+
   const createTableRow = function(row) {
 
     const $title = $("<h6>")
@@ -61,6 +135,14 @@ $(document).ready(function() {
     return $row;
   };
 
+  //created for test
+  const renderAccordion = (lists, path, container) => {
+    for (let i = 0; i < lists.length; i++) {
+      const $newPoint = createAccordionCard(lists[i], i, path, container);
+      container.append($newPoint);
+    }
+  };
+
   const renderTableItems = function(lists, container) {
     for (const list of lists) {
       const $newPoint = createTableRow(list);
@@ -68,17 +150,21 @@ $(document).ready(function() {
     }
   };
 
+  //adjusted for test
   const loadTableItems = function(container, path) {
     container.empty();
     $.get("/lists", (data) => {
-      renderTableItems(data[path], container);
+      (path === "favs")
+        ? renderAccordion(data[path], path, container)
+        : renderTableItems(data[path], container);
     });
   };
 
+  //adjusted for test
   const renderTables = function() {
     //Loop through lists to render on homepage
     const paths = ["favs", "myMaps", "myContributions", "allMaps"];
-    const containers = [$("#favs-container"), $("#my-maps-container"), $("#my-contributions-container"), $("#all-maps-container") ];
+    const containers = [$("#favs-accordion"), $("#my-maps-container"), $("#my-contributions-container"), $("#all-maps-container") ];
 
     for (let i = 0; i < paths.length; i++) {
       loadTableItems(containers[i], paths[i]);
