13e6ca9c24b2a076ff0b109ff0d5ac3264f25180
Elissa <elissa.matsushita@gmail.com>
2020-02-14 01:32:53 +0000

    fix list routing for  my contributions

--- db/seeds/02_lists_seeds.sql
+++ db/seeds/02_lists_seeds.sql
@@ -6,4 +6,6 @@ VALUES
 (4, 'Best Bootcamps in Vancouver', 'There is only one location on this map!'),
 (5, 'Favourite beaches', 'These are the best beaches in Vancouver'),
 (1, 'Cheap Houses in Vancouver', 'Hahaha j/k there are none'),
-(4, 'Best Ramen in Vancouver', 'Great for quick eats!');
+(4, 'Best Ramen in Vancouver', 'Great for quick eats!'),
+(2, 'Pools in Vancouver', 'For when I feel like swimming.'),
+(3, 'Vancouver Attractions', 'Things to do in Vancouver');
